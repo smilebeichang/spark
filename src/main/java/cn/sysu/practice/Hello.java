9f263ca63b431fc2e449ed25b84ce701c9be4434
songbeichang <songbeichang@163.com>
2021-07-13 16:35:10 +0800

    提交gitee2.0

---
+++ src/main/java/cn/sysu/practice/Hello.java
@@ -0,0 +1,15 @@
+package cn.sysu.practice;
+
+/**
+ * @Author : song bei chang
+ * @create 2021/7/13 16:34
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello gitee");
+    }
+}
+
+
+
