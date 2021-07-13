bbfb2d8f2abc4c8cec49686d9ab3e04951f2b1c6
songbeichang <songbeichang@163.com>
2021-07-13 16:51:19 +0800

    提交gitee4.0

--- src/main/java/cn/sysu/practice/Hello.java
+++ src/main/java/cn/sysu/practice/Hello.java
@@ -9,6 +9,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello gitee");
         System.out.println("hello origin");
+        System.out.println("888 origin");
     }
 }
 
